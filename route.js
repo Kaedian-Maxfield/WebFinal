53fc265e836613879705a26fb638b5d66ccb56bb
Joel S <salmer168@hotmail.com>
2019-02-27 11:53:41 -0700

    Create.pug has been added

--- route.js
+++ route.js
@@ -1,7 +1,11 @@
-
-
 exports.index = function(req, res){
     res.render('home', {
         "title":'Home',
+    });
+}
+
+exports.create = function(req,res){
+    res.render('create', {
+        title: 'Create Account'
     });
 }
