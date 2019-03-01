c15057308e03ce391d77f4eed7d13985df9a88c6
Brendan <btcleveland@gmail.com>
2019-03-01 11:30:59 -0700

    Messages dynamic

--- route.js
+++ route.js
@@ -1,6 +1,21 @@
 exports.index = function(req, res){
+    let messages = [
+        {username:'Newuser1',
+        messageContents:"Hello, I'm user 1 nice to meet you!",
+        timestamp:"11:20 AM"
+        },
+        {username:'NewUserNumber2',
+            messageContents:"hi, i am a different message",
+            timestamp:"11:25 AM"
+        },
+        {username:'Newuser1',
+        messageContents:"Nice to meet you, NewUserNumber2!",
+        timestamp:"11:26 AM"
+        }
+    ];
     res.render('home', {
         "title":'Home',
+        "messages":messages
     });
 }
 
