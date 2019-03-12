aadb6b30ce0ad057137d9ca3565cf275a6047d9e
Joel S <salmer168@hotmail.com>
2019-03-11 22:22:31 -0600

    Avatars are working

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ app.post("/", route.onIndex);
 app.get('/create', route.create);
 app.post('/create', route.onCreate);
 
-app.get('/edit', route.edit);
-app.post('/edit/:id', urlencodedParser, route.editUser);
+app.get('/edit/:id', route.edit);
+app.post('/edit/:id', route.editUser);
 
 app.get('/login', route.login);
 app.post('/login', route.onLogin);
